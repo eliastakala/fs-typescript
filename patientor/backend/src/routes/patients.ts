08478ea5a8585f5cba06de21fe8b6615aa54b58c
eliastakala <etakala1998@gmail.com>
2026-06-04 22:55:26 +0300

    feat/adding a new patient works

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,15 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from "express";
 import patientService from "../services/patientService.ts";
 
 const router = express.Router();
 
 router.get("/", (_req, res) => {
-  const data = patientService.getEntries();
+  const data = patientService.getPatients();
   res.send(data);
 });
 
-router.post("/", (_req, res) => {
-  res.send("Saving a patient");
+router.post('/', (req, res) => {
+  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
+  const addedPatient = patientService.addPatient({    
+    name,
+    dateOfBirth,
+    ssn,
+    gender,
+    occupation
+  });  
+  res.json(addedPatient);
 });
+
+
 
 export default router;
--- patientor/backend/src/services/patientService.ts
+++ patientor/backend/src/services/patientService.ts
@@ -1,16 +1,37 @@
-import data from '../../data/patients.ts';
-import type { NonSensitivePatientData } from '../../types.ts';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import data from "../../data/patients.ts";
+import type {
+  NonSensitivePatientData,
+  Patient,
+  NewPatient,
+} from "../../types.ts";
+import { v1 as uuid } from "uuid";
 
-const getEntries = (): NonSensitivePatientData[] => {
-  return data.map(({id, name, dateOfBirth, gender, occupation}) => ({
+const patients = data;
+
+const getPatients = (): NonSensitivePatientData[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
     id,
     name,
     dateOfBirth,
     gender,
-    occupation
+    occupation,
   }));
 };
 
+const addPatient = (entry: NewPatient): Patient => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  const personId: string = uuid();
+  const newPatient = {
+    id: personId,
+    ...entry,
+  };
+  console.log("new patient", newPatient);
+
+  patients.push(newPatient);
+  return newPatient;
+};
 export default {
-  getEntries
+  getPatients,
+  addPatient,
 };
